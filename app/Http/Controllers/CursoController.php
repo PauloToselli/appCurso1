b9fe3684658aa1819dbddd09a6aa9d560091f464
PauloToselli <116695525+PauloToselli@users.noreply.github.com>
2023-10-23 20:21:22 -0300

    commit1

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -2,11 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request;
+
+use App\Models\Curso;
+
+
 
 class CursoController extends Controller
 {
     public function mostrarFormCurso(){
         return view('cad_curso');
+    }
+
+    public function cadastroCurso(Request $request){
+        //verifica se existe algo na variável nomecategoria
+        dd($request);
+       $registrosCurso = $request->validate([
+        'nomecurso' => 'string|required',
+        'cargahoraria' => 'string|required',
+        'idcategoria' => 'numeric|required',
+        'valor' => 'numeric|required',
+    
+       ]);
+       //dd($regidtrosCursos)
+       // Esta linha é que grava o registro no banco.
+       Categoria::create($registrosCurso);
+
+       return Redirect::route('index');
+
     }
 }
